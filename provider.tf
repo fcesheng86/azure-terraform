1678cfa26157214ef9a78bdc5d3b9152651a8d16
fcesheng86 <foo.nicholas@gmail.com>
2023-12-23 18:45:03 +0800

    init commit for vnet

---
+++ provider.tf
@@ -0,0 +1,18 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  # subscription_id            = "5b79d5a5-399e-439f-8baf-a8ca1654cc3b"
+  # tenant_id                  = "a6fadded-8a88-471f-a9f1-4fbd7bb0f321"
+  skip_provider_registration = true
+  features {
+  }
+}

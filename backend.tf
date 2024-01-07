ba81c4c22b02971e3f67f07f4de33ca7f15adc01
fcesheng86 <foo.nicholas@gmail.com>
2024-01-07 19:19:25 +0800

    updating pipeline and provider

---
+++ backend.tf
@@ -0,0 +1,15 @@
+# terraform {
+#   required_version = ">= 1.3"
+#   backend "azurerm" {
+#     resource_group_name  = "kopicloud-tfstate-rg"
+#     storage_account_name = "kopicloudiactest"
+#     container_name       = "core-tfstate"
+#     key                  = "actions.tfstate"
+#   }
+#   required_providers {
+#     azurerm = {
+#       version = "~>3.2"
+#       source  = "hashicorp/azurerm"
+#     }
+#   }
+# }
